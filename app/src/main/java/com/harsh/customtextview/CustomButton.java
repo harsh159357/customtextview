d5ffb3aa765f91bd240ab6d262d1c2377ff6432c
HARSH SHARMA <harsh159357@gmail.com>
2017-04-19 23:57:56 +0530

    CustomButton Creation

---
+++ app/src/main/java/com/harsh/customtextview/CustomButton.java
@@ -0,0 +1,53 @@
+package com.harsh.customtextview;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+@SuppressLint("AppCompatCustomView")
+public class CustomButton extends Button {
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomButton(Context context) {
+        super(context);
+        init(null);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView);
+            String fontName = a.getString(R.styleable.CustomView_font);
+
+            try {
+                if (fontName != null) {
+                    Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                    setTypeface(myTypeface);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            a.recycle();
+        }
+    }
+}
